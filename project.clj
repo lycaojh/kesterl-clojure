d434c477b4de1d0241482f253d09a1c44e65f92f
Junhui Cao <lycaojh@gmail.com>
2013-02-21 11:58:24 +0800

    add project.clj

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject kestrel-clojure "0.1.0-SNAPSHOT"
+      :dependencies [[org.clojure/clojure "1.4.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]
+                     [org.clojure/tools.logging "0.2.3"]
+                     [log4j "1.2.17" :exclusions[javax.mail/mail
+                                                 javax.jms/jms
+                                                 com.sun.jdmk/jmxtools
+                                                 com.sun.jmx/jmxri]]
+                     [clj-xmemcached "0.1.1"]
+                     [com.googlecode.xmemcached/xmemcached "1.3.5"]]
+      :profiles
+      {:dev {:dependencies [
+                            [log4j/log4j "1.2.16"]
+                            [org.slf4j/slf4j-log4j12 "1.5.6"]]}})
